6d1ef18fbbd07f4cf5341071d669404260144f1e
thleima <thomas61.amiel@gmail.com>
2025-01-09 00:03:47 +0100

    start cleaning

--- index.js
+++ index.js
@@ -10,41 +10,53 @@ function handleSearch() {
 	fetch("travel_recommendation.json")
 		.then((response) => response.json())
 		.then((data) => {
-			const country = data.countries.find(
-				(item) => item.name.toLowerCase() === input
-			);
-			if (country) {
-				for (let city of country.cities) {
-					results.innerHTML += createCard(
-						city.imageUrl,
-						city.name,
-						city.description
-					);
-				}
-			} else if (input === "temples" || input === "temple") {
-				for (let temple of data.temples) {
-					results.innerHTML += createCard(
-						temple.imageUrl,
-						temple.name,
-						temple.description
-					);
-				}
-			} else if (input === "beaches" || input === "beach") {
-				for (let beach of data.beaches) {
-					results.innerHTML += createCard(
-						beach.imageUrl,
-						beach.name,
-						beach.description
-					);
-				}
-			} else {
-				results.innerHTML += "No results found";
-			}
+			checkCountry(data, input);
+			checkTemples(data, input);
+			checkBeaches(data, input);
 		})
 		.catch((error) => {
 			console.log("Error fetching data : ", e);
 			results.innerHTML += "Error";
 		});
+}
+
+function checkCountry(data, input) {
+	const country = data.countries.find(
+		(item) => item.name.toLowerCase() === input
+	);
+	if (country) {
+		for (let city of country.cities) {
+			results.innerHTML += createCard(
+				city.imageUrl,
+				city.name,
+				city.description
+			);
+		}
+	}
+}
+
+function checkTemples(data, input) {
+	if (input === "temples" || input === "temple") {
+		for (let temple of data.temples) {
+			results.innerHTML += createCard(
+				temple.imageUrl,
+				temple.name,
+				temple.description
+			);
+		}
+	}
+}
+
+function checkBeaches(data, input) {
+	if (input === "beach" || input === "beaches") {
+		for (let beach of data.beaches) {
+			results.innerHTML += createCard(
+				beach.imageUrl,
+				beach.name,
+				beach.description
+			);
+		}
+	}
 }
 
 function createCard(imgUrl, name, description) {
